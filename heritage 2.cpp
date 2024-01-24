1ab29a61d0c6c937f5b15951c93ac3d9eaa780b4
prince-buha <150029430+prince-buha@users.noreply.github.com>
2024-01-24 16:23:43 +0530

    Update heritage 2.cpp

--- heritage 2.cpp
+++ heritage 2.cpp
@@ -1,3 +1,11 @@
+/*
+
+2. WAP with a mother class and an inherited daugther class.Both of them should have a method void display ()that prints a message (different for mother and daugther). 
+In the main define a daughter and call the display() method on it.
+
+The method from Mother Class
+The method from Daughter Class
+*/
 #include<iostream>
 using namespace std;
 class Mother{
